4c516f73ce184e2759d1df863d41cb40ef35465b
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-10-22 02:14:33 -0500

    fix this so it actually works, add pod

--- lib/DBIx/Class/Helper/SubClass.pm
+++ lib/DBIx/Class/Helper/SubClass.pm
@@ -16,12 +16,23 @@ sub generate_relationships {
    foreach my $rel ($self->relationships) {
       my $rel_info = $self->relationship_info($rel);
       my $class = $rel_info->{class};
-      if ($class =~ m/([A-Za-z0-9_:]+)::Result::([A-Za-z0-9_]+)/) {
-         warn $1;
-         warn $2;
+
+      my ($namespace, $result);
+
+      if ($self =~ m/([A-Za-z0-9_:]+)::Result::[A-Za-z0-9_]+/) {
+         $namespace = $1;
       } else {
-         die qq{$class doesn't look like}.'${namespace}::Result::$resultclass';
+         die "$self doesn't look like".'${namespace}::Result::$resultclass';
       }
+
+      if ($class =~ m/[A-Za-z0-9_:]+::Result::([A-Za-z0-9_]+)/) {
+         $result = $1;
+      } else {
+         die "$class doesn't look like".'${namespace}::Result::$resultclass';
+      }
+
+      $class = $namespace . '::Result::' . $result;
+
       $self->add_relationship(
          $rel,
          $class,
@@ -45,20 +56,25 @@ sub set_table {
 =head1 DESCRIPTION
 
 This component is to allow simple subclassing of L<DBIx::Class> Result classes.
-Unfortunately, the parent classes are "Abstract" in that they can't be used on
-their own without a little bit of extra work.
 
 =head1 METHODS
 
 =head2 subclass
 
 This is probably the method you want.  You call this in your child class and it
-"imports" the definitions from the parent into itself.
+imports the definitions from the parent into itself.
 
 =head2 generate_relationships
+
+This is where the cool stuff happens.  This assumes that the namespace is laid
+out in the recommended C<MyApp::Schema::Result::Foo> format.  If the parent has
+C<Parent::Schema::Result::Foo> related to C<Parent::Schema::Result::Bar>, and you
+inherit from C<Parent::Schema::Result::Foo> in C<MyApp::Schema::Result::Foo>, you
+will automatically get the relationship to C<MyApp::Schema::Result::Bar>.
+
 =head2 set_table
 
-This is a super basic method that just sets the current classes table to the
-parent classes table.
+This is a super basic method that just sets the current classes' table to the
+parent classes' table.
 
 =end
