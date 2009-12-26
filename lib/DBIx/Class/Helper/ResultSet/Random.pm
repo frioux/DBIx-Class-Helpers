72374237c5dfafa987c04f108f48c0c399590a51
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-12-26 11:39:48 -0600

    random rows yo

--- lib/DBIx/Class/Helper/ResultSet/Random.pm
+++ lib/DBIx/Class/Helper/ResultSet/Random.pm
@@ -3,12 +3,26 @@ package DBIx::Class::Helper::ResultSet::Random;
 use strict;
 use warnings;
 
+use parent 'DBIx::Class::Helper::ResultSet::Union';
+
 # ABSTRACT: Get random rows from a ResultSet
+
+# this is ghetto
+my %rand_order_by = (
+   'DBIx::Class::Storage::DBI::SQLite'                     => 'RANDOM()',
+   'DBIx::Class::Storage::DBI::mysql'                      => 'RANDOM()',
+   'DBIx::Class::Storage::DBI::ODBC::Microsoft_SQL_Server' => 'RAND()',
+   'DBIx::Class::Storage::DBI::MSSQL'                      => 'RAND()',
+   'DBIx::Class::Storage::DBI::Pg'                         => 'RAND()',
+   'DBIx::Class::Storage::DBI::Oracle'                     => 'dbms_random.value',
+);
+
+sub rand_order_by {
+   return $rand_order_by{ref shift->result_source->storage} || 'RANDOM()';
+}
 
 sub rand {
    my $self   = shift;
-   $self->load_components(qw{Helper::ResultSet::Union});
-
    my $amount = shift || 1;
 
    $self->throw_exception('rand can only return a positive amount of rows')
@@ -17,25 +31,9 @@ sub rand {
    $self->throw_exception('rand can only return an integer amount of rows')
       unless $amount == int $amount;
 
-   if ($amount == 1) {
-      return $self->slice( int rand $self->count );
-   } else {
-      my $count = $self->count;
+   my $order_by = $self->rand_order_by;
 
-      $self->throw_exception('rand cannot select more rows than exist')
-         unless $amount <= $count;
-
-      my %rows;
-
-      while ($amount) {
-         next if $rows{1 + int rand $count}++;
-         $amount--;
-      }
-
-      return $self->union([
-         map $self->slice($_), keys %rows
-      ]);
-   }
+   return $self->search(undef, { rows=> $amount, order_by => \$order_by});
 }
 
 1;
@@ -65,6 +63,17 @@ This component allows convenient selection of random rows.
 
 =head2 rand
 
-Currently this method will return a ResultSet containing a single random row
-from the given ResultSet.  In the future it will take an argument of how many
-random rows should be included in the ResultSet.
+This method takes a single argument, being the size of the random ResultSet
+to return.  It defaults to 1.  This Component will throw exceptions if the
+argument is not an integer or not greater than zero.
+
+=head2 rand_order_by
+
+This module currently does an C<ORDER BY> on some db specific function.  If for
+some reason it guesses incorrectly for your database the easiest way to fix
+that in the short-term (ie without patching upstream) is to override this
+method.  So for example, if your db uses C<RAND()> instead of C<RANDOM()> and
+it's not in the predefined list of dbs you could just do the following in your
+ResultSet class:
+
+ sub rand_order_by { 'RAND()' }
--- t/05-random.t
+++ t/05-random.t
@@ -25,8 +25,11 @@ my $row = $schema->resultset('Foo')->rand->single;
 # so suck it.
 ok $row->id >= 1 && $row->id <= 5, 'row is one of the rows from the database';
 
-use Devel::Dwarn;
-my @rows = Dwarn map $_->id, $schema->resultset('Foo')->rand(4)->all;
+my @rows = map $_->id, $schema->resultset('Foo')->rand(4)->all;
+ok @rows == 4, 'correct amount of rows selected';
+for (@rows) {
+   ok $_ >= 1 && $_ <= 5, 'row is one of the rows from the database';
+}
 
 
 done_testing;
