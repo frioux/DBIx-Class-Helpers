20a79640480013ea1c1a85d6d4c75c724da4bf55
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-12-29 11:21:41 -0600

    privatization is the future

--- lib/DBIx/Class/Helper/ResultSet/Random.pm
+++ lib/DBIx/Class/Helper/ResultSet/Random.pm
@@ -17,7 +17,7 @@ my %rand_order_by = (
    'DBIx::Class::Storage::DBI::Oracle'                     => 'dbms_random.value',
 );
 
-sub rand_order_by {
+sub _rand_order_by {
    return $rand_order_by{ref shift->result_source->storage} || 'RAND()';
 }
 
@@ -31,7 +31,7 @@ sub rand {
    $self->throw_exception('rand can only return an integer amount of rows')
       unless $amount == int $amount;
 
-   my $order_by = $self->rand_order_by;
+   my $order_by = $self->_rand_order_by;
 
    return $self->search(undef, { rows=> $amount, order_by => \$order_by});
 }
@@ -82,7 +82,7 @@ This method takes a single argument, being the size of the random ResultSet
 to return.  It defaults to 1.  This Component will throw exceptions if the
 argument is not an integer or not greater than zero.
 
-=head2 rand_order_by
+=head2 _rand_order_by
 
 This module currently does an C<ORDER BY> on some db specific function.  If for
 some reason it guesses incorrectly for your database the easiest way to fix
@@ -91,4 +91,4 @@ method.  So for example, if your db uses C<RAND()> instead of C<RANDOM()> and
 it's not in the predefined list of dbs you could just do the following in your
 ResultSet class:
 
- sub rand_order_by { 'RAND()' }
+ sub _rand_order_by { 'RAND()' }
