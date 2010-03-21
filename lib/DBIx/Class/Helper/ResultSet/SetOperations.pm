ff10d6d2144b9e12a5425824e7d1694c37018c50
Yuval Kogman <nothingmuch@woobling.org>
2010-03-21 11:21:06 +0200

    Add all set operations

---
+++ lib/DBIx/Class/Helper/ResultSet/SetOperations.pm
@@ -0,0 +1,181 @@
+package DBIx::Class::Helper::ResultSet::SetOperations;
+
+use strict;
+use warnings;
+
+# ABSTRACT: Do set operations with DBIx::Class
+
+# cribbed from perlfaq4
+sub _compare_arrays {
+   my ($self, $first, $second) = @_;
+
+   no warnings; # silence spurious -w undef complaints
+   return 0 unless @$first == @$second;
+   for (my $i = 0; $i < @$first; $i++) {
+      return 0 if $first->[$i] ne $second->[$i];
+   }
+   return 1;
+}
+
+sub union {
+   shift->_set_operation( UNION => @_ );
+}
+
+sub union_all {
+   shift->_set_operation( "UNION ALL" => @_ );
+}
+
+sub intersect {
+   shift->_set_operation( INTERSECT => @_ );
+}
+
+sub intersect_all {
+   shift->_set_operation( "INTERSECT ALL" => @_ );
+}
+
+sub _except_keyword {
+   my $self = shift;
+
+   $self->{_except_keyword} ||= ( $self->result_source->schema->storage->sqlt_type eq 'Oracle' ? "MINUS" : "EXCEPT" );
+}
+
+sub except {
+   my ( $self, @args ) = @_;
+   $self->_set_operation( $self->_except_keyword => @args );
+}
+
+sub except_all {
+   # not supported on most DBs
+   shift->_set_operation( "EXCEPT ALL" => @_ );
+}
+
+sub _set_operation {
+   my ( $self, $operation, $other ) = @_;
+
+   my @sql;
+   my @params;
+
+   my $as = $self->_resolved_attrs->{as};
+
+   my @operands = ( $self, ref $other eq 'ARRAY' ? @$other : $other );
+
+   for (@operands) {
+      $self->throw_exception('ResultClass of queries passed to union do not match!')
+         unless ref $self->_result_class eq ref $_->_result_class;
+
+      my $attrs = $_->_resolved_attrs;
+
+      $self->throw_exception('ResultSets do not all have the same selected columns!')
+         unless $self->_compare_arrays($as, $attrs->{as});
+
+      my ($sql, $bind) = $self->result_source->storage->_select_args_to_query(
+         $attrs->{from}, $attrs->{select}, $attrs->{where}, $attrs
+      );
+
+      push @sql, $sql;
+      push @params, @{$bind};
+   }
+
+   my $query = q<(> . join(" $operation ", @sql). q<)>;
+
+   my $attrs = $self->_resolved_attrs;
+   my $new_rs = $self->result_source->resultset->search(undef, {
+      alias => $self->current_source_alias,
+      from => [{
+         $self->current_source_alias => \[ $query, @params ],
+         -alias                      => $self->current_source_alias,
+         -source_handle              => $self->result_source->handle,
+      }],
+      columns => $attrs->{as},
+   });
+
+   $new_rs->result_class($self->result_class);
+
+   return $new_rs;
+}
+
+1;
+
+=pod
+
+=head1 SYNOPSIS
+
+ package MyApp::Schema::ResultSet::Foo;
+
+ __PACKAGE__->load_components(qw{Helper::ResultSet::SetOperations});
+
+ ...
+
+ 1;
+
+And then elsewhere, like in a controller:
+
+ my $rs1 = $rs->search({ foo => 'bar' });
+ my $rs2 = $rs->search({ baz => 'biff' });
+ for ($rs1->union($rs2)->all) { ... }
+
+=head1 DESCRIPTION
+
+This component allows you to create unions with your ResultSets.  See
+L<DBIx::Class::Helper::ResultSet/NOTE> for a nice way to apply it to your
+entire schema.
+
+Component throws exceptions if ResultSets have different ResultClasses or
+different "Columns Specs."
+
+The basic idea here is that in SQL if you union two queries they must be
+selecting the same columns names, so that the results will all match.  The
+deal with the ResultClasses is that DBIC needs to inflate the results the same
+for the entire ResultSet, so if one were to try to union in a table with the
+same column name but different classes DBIC wouldn't be doing what you would
+expect.
+
+A nice way to use this is with L<DBIx::Class::ResultClass::HashRefInflator>.
+
+So you might have something like the following sketch autocompletion code:
+
+ my $rs1 = $schema->resultset('Album')->search({
+    name => { -like => "$input%" }
+ }, {
+   columns => [qw{id name}],
+   '+select' => [\'"album" AS tablename']
+ });
+
+ my $rs2 = $schema->resultset('Artist')->search({
+    name => { -like => "$input%" }
+ }, {
+   columns => [qw{id name}],
+   '+select' => [\'"artist" AS tablename']
+ });
+
+ my $rs3 = $schema->resultset('Song')->search({
+    name => { -like => "$input%" }
+ }, {
+   columns => [qw{id name}],
+   '+select' => [\'"song" AS tablename']
+ });
+
+ $_->result_class('DBIx::Class::ResultClass::HashRefInflator')
+   for ($rs1, $rs2, $rs3);
+
+ my $data = [$rs1->union([$rs2, $rs3])->all];
+
+=head1 METHODS
+
+=head2 union
+
+=head2 union_all
+
+=head2 intersect
+
+=head2 intersect_all
+
+=head2 except
+
+=head2 except_all
+
+All of these methods take a single ResultSet or an ArrayRef of ResultSets as
+the parameter only parameter.
+
+On Oracle C<except> will issue a C<MINUS> operation.
+
---
+++ t/07-set_operations.t
@@ -0,0 +1,92 @@
+#!perl
+
+use strict;
+use warnings;
+
+use lib 't/lib';
+use Test::More;
+use Test::Deep;
+use Test::Exception;
+
+use TestSchema;
+my $schema = TestSchema->deploy_or_connect();
+$schema->prepopulate;
+
+my $rs = $schema->resultset('Foo')->search({ id => 1 });
+my $rs2 = $schema->resultset('Foo')->search({ id => { '>=' => 3 } });
+my $rs3 = $schema->resultset('Foo')->search({ id => [ 1, 3 ] });
+
+cmp_deeply [ sort map $_->id, $rs2->union($rs2)->all ], [3, 4, 5],
+   'union returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs2->union_all($rs2)->all ], [3, 3, 4, 4, 5, 5],
+   'union returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs2->union($rs)->all ], [1, 3, 4, 5],
+   'union returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs3->union($rs)->all ], [1, 3],
+   'union returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs3->union_all($rs)->all ], [1, 1, 3],
+   'union returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs2->intersect($rs)->all ], [],
+   'intersect returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs3->intersect($rs)->all ], [1],
+   'intersect returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs->intersect($rs3)->all ], [1],
+   'intersect returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs2->intersect($rs3)->all ], [3],
+   'intersect returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs3->intersect($rs2)->all ], [3],
+   'intersect returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs2->except($rs)->all ], [3, 4, 5],
+   'except returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs->except($rs2)->all ], [1],
+   'except returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs3->except($rs)->all ], [3],
+   'except returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs->except($rs3)->all ], [],
+   'except returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs2->except($rs3)->all ], [4, 5],
+   'except returns correct values';
+
+cmp_deeply [ sort map $_->id, $rs3->except($rs2)->all ], [1],
+   'except returns correct values';
+
+dies_ok {
+   my $rs3 = $rs->search(undef, { columns => ['id'] });
+   $rs->union($rs3) ;
+} 'unioning differing ColSpecs dies';
+
+dies_ok { $rs->union($schema->resultset('Bar')) } 'unioning differing ResultSets dies';
+
+{
+   my $rs3 = $rs->search(undef, {
+      columns => ['id'],
+      '+select' => [\'"foo" as station'],
+      '+as'     => ['station'],
+   });
+   my $rs4 = $schema->resultset('Bar')->search(undef, {
+      columns => ['id'],
+      '+select' => [\'"bar" as station'],
+      '+as'     => ['station'],
+   });
+   $rs3->result_class('DBIx::Class::ResultClass::HashRefInflator');
+   $rs4->result_class('DBIx::Class::ResultClass::HashRefInflator');
+   my $rs5 = $rs3->union($rs4);
+   lives_ok { [ $rs5->all ] }
+      q{unioning differing ResultSets does not die when you know what you're doing};
+}
+
+done_testing;
