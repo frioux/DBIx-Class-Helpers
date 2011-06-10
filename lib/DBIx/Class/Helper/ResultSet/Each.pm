d65bb5528f7a699d997de662bcbfb8ee17afda26
John Napiorkowski <jjn1056@yahoo.com>
2011-06-10 14:42:01 -0400

    moved class to its own file;

--- lib/DBIx/Class/Helper/ResultSet/Each.pm
+++ lib/DBIx/Class/Helper/ResultSet/Each.pm
@@ -4,7 +4,7 @@ package DBIx::Class::Helper::ResultSet::Each;
 
 use strict;
 use warnings;
-#use DBIx::Class::Helpers::Util::ResultSetItr;
+use DBIx::Class::Helpers::Util::ResultSetItr;
 
 sub each {
   my($self, $func, $fail) = @_;
@@ -25,95 +25,6 @@ sub each {
 
   return $self;
 }
-
-package DBIx::Class::Helpers::Util::ResultSetItr;
-
-use strict;
-use warnings;
-
-sub new {
-  my ($class, %args) = @_;
-  bless(\%args, $class);
-}
-
-sub index { shift->{index} }
-sub _inc_index { shift->{index}++ }
-sub _init_index { shift->{index} ||= 0 }
-sub _has_index { defined shift->{index} }
-sub _init_or_inc_index {
-  my $self = shift;
-  $self->_has_index  ?
-    $self->_inc_index : $self->_init_index;
-}
-
-sub count { shift->index + 1 }
-
-sub escape { shift->{escape} = 1 }
-sub has_escaped { shift->{escape} ? 1:0 }
-sub has_not_been_used { defined shift->{index} ? 0:1 }
-
-sub is_first { shift->index == 0 ? 1:0 }
-sub is_not_first { shift->index == 0 ? 0:1 }
-
-sub is_even { shift->index % 2 ? 1:0 }
-sub is_odd { shift->index % 2 ? 0:1 }
-sub resultset { shift->{resultset} }
-
-sub first {
-  my ($self, $code, $fail) = @_;
-  if($self->is_first) {
-      $code->($self);
-  } elsif($fail) {
-      $fail->($self);
-  }
-  return $self;
-}
-
-sub not_first {
-  my ($self, $code, $fail) = @_;
-  if($self->is_not_first) {
-      $code->($self);
-  } elsif($fail) {
-      $fail->($self);
-  }
-  return $self;
-}
-
-sub even {
-  my ($self, $code, $fail) = @_;
-  if($self->is_even) {
-      $code->($self);
-  } elsif($fail) {
-      $fail->($self);
-  }
-  return $self;
-}
-
-sub odd {
-  my ($self, $code, $fail) = @_;
-  if($self->is_odd) {
-      $code->($self);
-  } elsif($fail) {
-      $fail->($self);
-  }
-  return $self;
-}
-
-sub next {
-  my $self = shift;
-  if(my $next = $self->resultset->next) {
-    $self->_init_or_inc_index;
-    return $next;
-  } else {
-    return;
-  }
-}
-
-## Possible, but requires $rs->count, and don't want that penalty for now
-
-sub last {}
-sub size {}
-sub max {}
 
 1;
 
