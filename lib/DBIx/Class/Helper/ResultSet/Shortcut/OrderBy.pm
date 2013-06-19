9fec20c841b5e54de9523cec3848393338428036
moltar <moltar@moltar.net>
2013-06-19 16:55:33 -0400

    Added functionality and docs for free form order by clause.

--- lib/DBIx/Class/Helper/ResultSet/Shortcut/OrderBy.pm
+++ lib/DBIx/Class/Helper/ResultSet/Shortcut/OrderBy.pm
@@ -3,6 +3,27 @@ package DBIx::Class::Helper::ResultSet::Shortcut::OrderBy;
 use strict;
 use warnings;
 
-sub order_by { shift->search(undef, { order_by => shift }) }
+sub order_by {
+    my ($self, @order) = @_;
+
+    ## pass thru if we have a HashRef format
+    if (@order && ref($order[0]) eq 'HASH') {
+        return $self->search(undef, { order_by => $order[0] });
+    }
+
+    my @clauses;
+    foreach (@order) {
+        foreach my $col (split(/\s*,\s*/)) {
+            my $dir = 'asc';
+            if (substr($col, 0, 1) eq '!') {
+                $col = substr($col, 1); # take everything after '!'
+                $dir = 'desc';
+            }
+            push @clauses, { "-$dir" => join('.', $self->current_source_alias, $col) };
+        }
+    }
+
+    return $self->search(undef, { order_by => \@clauses });
+}
 
 1;
