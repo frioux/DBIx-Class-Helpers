123fa4a165834901fcef56faa111fe7fc5ce7741
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-05-26 15:55:57 -0500

    factour out `A::Util`

--- t/lib/A/Role/TestConnect.pm
+++ t/lib/A/Role/TestConnect.pm
@@ -3,6 +3,8 @@ package A::Role::TestConnect;
 use Moo::Role;
 use TestSchema;
 
+use A::Util;
+
 has [qw(on_connect_call engine)] => ( is => 'ro' );
 
 has storage_type => (
@@ -11,27 +13,19 @@ has storage_type => (
    default => sub { shift->engine }
 );
 
-sub connected { !!@{shift->connect_info} }
+sub connected { A::Util::connected($_[0]->engine, $_[0]->on_connect_call) }
 
 has connect_info => (
    is => 'ro',
    lazy => 1,
    default => sub {
       my $self = shift;
-      my @connect_info = grep $_, map $ENV{$_}, $self->env_vars;
-      push @connect_info, { on_connect_call => $self->on_connect_call }
-         if @connect_info && $self->on_connect_call;
 
-      return \@connect_info;
+      A::Util::connect_info($self->engine, $self->on_connect_call)
    },
 );
 
-sub env_vars {
-   my $self = shift;
-
-   my $p = 'DBIITEST_' . uc($self->engine);
-   $p . '_DSN', $p . '_USER', $p . '_PASSWORD';
-}
+sub env_vars { A::Util::env(shift->engine) }
 
 has schema => (
    is => 'ro',
@@ -39,16 +33,7 @@ has schema => (
    builder => sub {
       my $self = shift;
 
-      my $schema = 'TestSchema';
-      $schema->storage_type('DBIx::Class::Storage::DBI'); # class methods: THE WORST
-      $schema->storage_type('DBIx::Class::Storage::DBI::' . $self->storage_type)
-         if $self->storage_type && !$self->connected;
-
-      $schema = TestSchema->connect(@{$self->connect_info});
-      $schema->deploy if $self->connected;
-      $schema->storage->dbh->{private_dbii_driver} = $self->engine;
-
-      $schema
+      A::Util::connect($self->engine, $self->storage_type, $self->on_connect_call)
    },
 );
 
---
+++ t/lib/A/Util.pm
@@ -0,0 +1,44 @@
+package A::Util;
+
+use strict;
+use warnings;
+
+use TestSchema;
+
+sub connect {
+   my ($engine, $storage_type, $on_connect_call) = @_;
+
+   my $schema = 'TestSchema';
+   $schema->storage_type('DBIx::Class::Storage::DBI'); # class methods: THE WORST
+   $schema->storage_type('DBIx::Class::Storage::DBI::' . $storage_type)
+      if $storage_type && !connected($engine, $on_connect_call);
+
+   $schema = TestSchema->connect(@{connect_info($engine, $on_connect_call)});
+   $schema->deploy if connected($engine, $on_connect_call);
+   $schema->storage->dbh->{private_dbii_driver} = $engine;
+
+   $schema
+}
+
+sub env {
+   my $engine = shift;
+
+   my $p = 'DBIITEST_' . uc($engine);
+   $p . '_DSN', $p . '_USER', $p . '_PASSWORD';
+}
+
+sub connect_info {
+   my ($engine, $on_connect_call) = @_;
+
+   my @connect_info = grep $_, map $ENV{$_}, env($engine);
+   push @connect_info, { on_connect_call => $on_connect_call }
+      if @connect_info && $on_connect_call;
+
+   return \@connect_info;
+}
+
+sub connected {
+   !!@{connect_info(@_)}
+}
+
+1;
