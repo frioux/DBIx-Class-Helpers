564c72869248f3d07a868060660c07cefb8a7b76
Peter Rabbitson <ribasushi@leporine.io>
2018-01-26 18:48:16 +0100

    Nuke not-applicable comment
    This was all written based on the first iteration, without a
    separate `results_exist_as_query`. No longer relevant.

--- lib/DBIx/Class/Helper/ResultSet/ResultsExist.pm
+++ lib/DBIx/Class/Helper/ResultSet/ResultsExist.pm
@@ -10,13 +10,6 @@ sub results_exist_as_query {
 
    my $reified = $self->get_column( \1 )->as_query;
 
-   # this would have been the "clean" way to do it
-   # alas we can not use it - we do not know what WHERE's a fresh
-   # resultset would have tacked on it via default_attrs
-   #$$reified->[0] = "EXISTS $$reified->[0]";
-   #
-   #( $self->result_source->resultset->get_column($reified)->all )[0];
-
    $$reified->[0] = "( SELECT EXISTS $$reified->[0] )";
 
    $reified;
