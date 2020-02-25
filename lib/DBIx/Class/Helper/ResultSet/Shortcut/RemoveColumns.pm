a8a692aa664d900bfbe00fd05f5edb73ebc4e52e
Karen Etheridge <ether@cpan.org>
2020-02-24 16:20:26 -0800

    oops, need to load the component that makes the attribute work

--- lib/DBIx/Class/Helper/ResultSet/Shortcut/RemoveColumns.pm
+++ lib/DBIx/Class/Helper/ResultSet/Shortcut/RemoveColumns.pm
@@ -5,6 +5,8 @@ use warnings;
 
 use parent 'DBIx::Class::ResultSet';
 
+__PACKAGE__->load_components('Helper::ResultSet::RemoveColumns');
+
 sub remove_columns { shift->search(undef, { remove_columns => shift }) }
 
 1;
