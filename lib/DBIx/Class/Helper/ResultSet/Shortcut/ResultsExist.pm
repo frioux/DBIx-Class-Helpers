12c366b5a78bea09e4faac655466f63d5fa999fc
Daniel Böhmer <post@daniel-boehmer.de>
2020-03-12 15:57:48 +0100

    Pass optional \%cond from results_exist() to search()

--- lib/DBIx/Class/Helper/ResultSet/Shortcut/ResultsExist.pm
+++ lib/DBIx/Class/Helper/ResultSet/Shortcut/ResultsExist.pm
@@ -6,10 +6,10 @@ use warnings;
 use parent 'DBIx::Class::ResultSet';
 
 sub results_exist_as_query {
-   my $self = shift;
+   my ($self, $cond) = @_;
 
 
-   my $reified = $self->search_rs( {}, {
+   my $reified = $self->search_rs( $cond, {
       columns => { _results_existence_check => \ '42' }
    } )->as_query;
 
@@ -22,9 +22,9 @@ sub results_exist_as_query {
 
 
 sub results_exist {
-   my $self = shift;
+   my ($self, $cond) = @_;
 
-   my $query = $self->results_exist_as_query;
+   my $query = $self->results_exist_as_query($cond);
    $$query->[0] .= ' AS _existence_subq';
 
    my( undef, $sth ) = $self->result_source
